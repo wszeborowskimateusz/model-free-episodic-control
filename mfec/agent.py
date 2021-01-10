e2480619a67100c13c7ce5c2b47014fa4f7a86e0
unknown <polstephenson@gmail.com>
2021-01-10 18:25:05 +0100

    First version of VAE

--- mfec/agent.py
+++ mfec/agent.py
@@ -94,3 +94,57 @@ class MFECAgent:
     def load(path):
         with open(path, "rb") as file:
             return pickle.load(file)
+
+class RandomAgent:
+    def __init__(
+        self,
+        buffer_size,
+        k,
+        discount,
+        epsilon,
+        height,
+        width,
+        state_dimension,
+        actions,
+        seed,
+    ):
+        self.rs = np.random.RandomState(seed)
+        self.size = (height, width)
+        self.memory = []
+        self.actions = actions
+        self.qec = QEC(self.actions, buffer_size, k)
+        self.projection = self.rs.randn(
+            state_dimension, height * width
+        ).astype(np.float32)
+        self.discount = discount
+        self.epsilon = epsilon
+
+        self.state = np.empty(state_dimension, self.projection.dtype)
+        self.action = int
+        self.time = 0
+
+    def choose_action(self, observation):
+        return random.choice(self.actions)
+
+    def receive_reward(self, reward):
+        self.memory.append(
+            {
+                "state": self.state,
+                "action": self.action,
+                "reward": reward,
+                "time": self.time,
+            }
+        )
+
+    def train(self):
+        value = 0.0
+
+    def save(self, results_dir):
+        with open(os.path.join(results_dir, "agent.pkl"), "wb") as file:
+            pickle.dump(self, file, 2)
+
+    @staticmethod
+    def load(path):
+        with open(path, "rb") as file:
+            return pickle.load(file)
+
